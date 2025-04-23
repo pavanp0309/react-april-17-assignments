37a1cdad5d544bfd1712cc43e586fc467ecf905b
Gowripriyankadokula <gowripriyankadokula@gmail.com>
2025-04-23 17:47:05 +0530

    completed variables@23-04-25@5:46pm

---
+++ priyanka/javascript/assignment.js
@@ -0,0 +1,60 @@
+//let age=20;
+//console.log(age)
+
+//let firstname="priyanka" , lastname="dokula" , country="anakapalle";
+//console.log(firstname, lastname, country);
+
+//let itemprice=15;
+//let itemquantity=3;
+//let totalcost = itemprice * itemquantity;
+//console.log("Total cost:", totalcost);
+
+//let name;
+//function greet() {
+ //   name = "Alice";
+ //   console.log("Hello, " + name);
+//}
+//greet();
+//console.log(name);  
+
+//var myvar;
+//console.log(myvar);
+//myvar=10;
+
+//console.log(myLet); // ReferenceError
+//let myLet = 10;
+
+
+//var message;
+//function myfunction() {
+//    message="hii everyone";
+//    console.log(message);
+//}
+
+//myfunction();
+//console.log(message);
+
+//console.log(myconst);
+//const myconst=10;
+
+//var myvar ="hello friends";
+//let myvar="hello everyone"; //Uncaught SyntaxError: Identifier 'myvar' has already been declared
+//console.log(myvar);
+
+//var myvar="hello friends";
+//function test() {
+//   let myvar="hello everyone";
+//    console.log(myvar);
+//}
+//test();
+//console.log(myvar);
+
+
+function test(){
+    const myconst="hello friends";
+    console.log(myconst);
+}
+test();
+const myconst="hello";
+console.log(myconst);
+
