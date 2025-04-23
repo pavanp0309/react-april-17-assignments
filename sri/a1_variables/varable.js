ddcba22fb5da12f48a1008aa96694cbbf95c627b
SRIKANTHSAIVARMA <srikanthraju531@gmail.com>
2025-04-23 18:27:05 +0530

    completed a1_variables@23/4/25@6:30

---
+++ sri/a1_variables/varable.js
@@ -0,0 +1,43 @@
+let age  =10
+console.log(age)
+let firstname="vasu";lastname="dev";country="india"
+console.log(firstname,lastname,country)
+const itemprice=50
+const itemquantity=5
+console.log(itemprice*itemquantity)
+//function vasu(){
+    let v = 12;
+    let d = 23;
+
+//
+/*console.log(v,d)
+console.log(g)
+console.log(h)
+var h=50
+let g =20
+*/
+/*
+function my(){
+    var t =20
+}
+my(t)
+*/
+/*
+var t =10
+
+let t = 20
+console.log(t)
+*/
+/*
+function my(){
+    const t=20
+    //let t=30
+}
+const t = 30
+console.log(t)*/
+
+function my(){
+    let vd = 20
+    var kd = 30
+    const jd =40
+}
